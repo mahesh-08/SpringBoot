5c991f6a61cc6931c8c198446fc04898837e7740
sharadrajore <sharad.rajore@gmail.com>
2020-04-16 12:30:05 +0530

    spring boot initilizer war

---
+++ spring-initiazer/src/main/java/com/zensar/SpringInitiazerApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitiazerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitiazerApplication.class, args);
+	}
+
+}
