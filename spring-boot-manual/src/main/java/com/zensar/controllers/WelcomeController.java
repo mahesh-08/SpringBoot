fe171d2eff72f258f856d681c89af3a575fd8f3c
sharadrajore <sharad.rajore@gmail.com>
2020-04-16 11:28:58 +0530

    spring boot manaul way

---
+++ spring-boot-manual/src/main/java/com/zensar/controllers/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.zensar.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+	
+	@RequestMapping("/")
+	public String sayHello(){
+		return "<h1> Welcome to Spring Boot </h1>"; 
+	}
+
+}
